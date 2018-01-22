d65e64b9a852b5f26593cdbae4e46a8013ab845e
Vadim <vadimka108@gmail.com>
2018-01-22 09:26:11 +0300

    add sql field aviable

--- dockerProduct/docker/db/dump.sql
+++ dockerProduct/docker/db/dump.sql
@@ -5,5 +5,6 @@ CREATE TABLE `Product` (
   `image` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
   `price` DECIMAL(12, 2) NOT NULL,
+  `aviable` int(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- migrations/00001_product.sql
+++ migrations/00001_product.sql
@@ -6,6 +6,7 @@ CREATE TABLE `Product` (
   `image` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
   `price` DECIMAL(12, 2) NOT NULL,
+  `aviable` int(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 -- +goose Down
